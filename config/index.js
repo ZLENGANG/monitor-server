49171fce993e52b5340c7cc3ae05983bfb3b07a8
roninz <1437004632@qq.com>
2024-07-21 10:23:14 +0800

    update

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    WIN_WIN_TOKEN: "02d14e4abecfe88431b272a61c17215dd50c6b03",
+    WIN_WIN_TOKEN: "1c725c1c96f5e40f0d11e65f48a4deee01d4d179",
     SEND_KEY: "SCT252192TnpgEYho5O5EPR0f4Fegi6jht"
 }
